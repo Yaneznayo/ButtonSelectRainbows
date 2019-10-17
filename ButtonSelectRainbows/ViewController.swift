b9c59ff410f7069e7f56df31e2a5e07cb154f139
1985wasagoodyear <1985wasagoodyear>
2019-10-17 09:52:07 -0400

    Added random color functionality hurray.

--- ButtonSelectRainbows/ViewController.swift
+++ ButtonSelectRainbows/ViewController.swift
@@ -16,9 +16,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    func randomNum() -> CGFloat {
+        return CGFloat(Int.random(in: 0...255))
+    }
 
     @IBAction func buttonAction(_ sender: Any) {
         button.setTitle("You touched meeee", for: .normal)
+        
+        // HTML -> hex, or 0-255, inclusive
+        // UIColor -> 0.0 -> 1.0
+        let randomColor = UIColor(red: randomNum() / 255.0,
+                                  green: randomNum() / 255.0,
+                                  blue: randomNum() / 255.0,
+                                  alpha: 1.0)
+        
+        button.backgroundColor = randomColor
     }
 }
 
